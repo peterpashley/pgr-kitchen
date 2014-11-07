c0baf689341e02f009ee40ae1ad159c43c0e30b9
peterpashley <peterpashley@gmail.com>
2014-11-07 01:06:03 +0000

    Added OptionSelector, toilet and glass

---
+++ Kitchen/Assets/Editor/OptionSelectorEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(OptionSelector))]
+public class OptionSelectorEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		OptionSelector selector = target as OptionSelector;
+
+		selector.cycleOptionKey = (KeyCode)EditorGUILayout.EnumPopup( "Cycle Key", selector.cycleOptionKey );
+
+		selector.optionText = (TextMesh)EditorGUILayout.ObjectField( selector.optionText, typeof(TextMesh), true );
+
+		int count = selector.options.Count;
+		string[] texts = new string[count];
+		for( int i=0 ; i<count ; i++ )
+		{
+			texts[i] = selector.options[i].name;
+		}
+		int newOption = GUILayout.SelectionGrid( selector.option, texts, 2 );
+		if( newOption != selector.option )
+		{
+			selector.option = newOption;
+			EditorUtility.SetDirty( selector );
+		}
+	}
+}
+
---
+++ Kitchen/Assets/OptionSelector.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class OptionSelector : MonoBehaviour 
+{
+	public KeyCode cycleOptionKey;
+	public TextMesh optionText;
+
+	[Range(0,20)]
+	public int option;
+
+	public List<Transform> options = new List<Transform>();
+
+	// Use this for initialization
+	void Start () 
+	{
+		FindOptions();
+		SetOption( option );
+	}
+
+	void SetOption( int value )
+	{
+		option = value;
+
+		if( optionText )
+		{
+			optionText.text = options[option].name;
+		}
+		
+		UpdateOptions();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if( !Application.isPlaying )
+		{
+			FindOptions();
+		}
+
+		if( cycleOptionKey != KeyCode.None && Input.GetKeyDown(cycleOptionKey) )
+		{
+			SetOption( (option+1)%options.Count );
+		}
+
+		UpdateOptions();
+	}
+
+	void FindOptions()
+	{
+		options.Clear();
+		
+		for( int i=0 ; i<transform.childCount ; i++ )
+		{
+			options.Add( transform.GetChild(i) );
+		}
+		
+		options.Sort( delegate(Transform x, Transform y) {
+			return x.name.CompareTo(y.name);
+		} );
+	}
+
+	void UpdateOptions () 
+	{
+		for( int i=0 ; i<options.Count ; i++ )
+		{
+			if( options[i].gameObject.activeSelf != (i==option) )
+			{
+				options[i].gameObject.SetActive( i==option);
+			}
+		}
+	}
+}
