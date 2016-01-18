292df8332c2f0023c5f5520295866958f6092e35
peterpashley <peterpashley@gmail.com>
2016-01-18 00:54:11 +0000

    Add KitchenOnly scene, and new system for layout

--- Kitchen/Assets/LayoutCuboid.cs
+++ Kitchen/Assets/LayoutCuboid.cs
@@ -26,19 +26,44 @@ public class WatchedVector3
 [ExecuteInEditMode]
 public class LayoutCuboid : MonoBehaviour 
 {
+	public enum VerticalPivot
+	{
+		Centre,
+		Base,
+	}
+
+	public VerticalPivot verticalPivot = VerticalPivot.Centre;
+
 	public WatchedVector3 edgeMin;
 	public WatchedVector3 edgeMax;
+	public Vector3 size;
+
+	Vector3 _lastPosition;
+	Quaternion _lastRotation;
 
 	// Use this for initialization
 	void Start () 
 	{
+		_lastPosition = transform.localPosition;
+		_lastRotation = transform.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		Vector3 nowPos = transform.localPosition;
+		if( _lastPosition != nowPos )
+		{
+			Vector3 delta = nowPos - _lastPosition;
+			edgeMax.value += delta;
+			edgeMin.value += delta;
+
+			_lastPosition = nowPos;
+		}
+
 		if(    edgeMin.Update() 
-			|| edgeMax.Update() )
+			|| edgeMax.Update() 
+			|| _lastRotation != transform.rotation )
 		{
 			UpdateMesh();
 		}
@@ -46,8 +71,24 @@ public class LayoutCuboid : MonoBehaviour
 
 	void UpdateMesh()
 	{
-		Vector3 size = edgeMax.value - edgeMin.value;
-		this.transform.localScale = size;
-		this.transform.position = 0.5f*(edgeMax.value + edgeMin.value);
+		size = edgeMax.value - edgeMin.value;
+		Vector3 scale = size;
+		float deltaAngle = Mathf.Abs(Mathf.DeltaAngle(this.transform.localRotation.eulerAngles.y, 0));
+		if( 45f < deltaAngle && deltaAngle < 135f)
+		{
+			float temp = scale.x;
+			scale.x = scale.z;
+			scale.z = temp;
+		}
+
+		this.transform.localScale = scale;
+		Vector3 pos = 0.5f*(edgeMax.value + edgeMin.value);
+		if( verticalPivot == VerticalPivot.Base )
+		{
+			pos.y = edgeMin.value.y;
+		}
+		this.transform.localPosition = pos;
+		_lastPosition = this.transform.localPosition;
+		_lastRotation = this.transform.rotation;
 	}
 }
---
+++ Kitchen/Assets/Measurement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System.Collections;
+
+public class Measurement : MonoBehaviour 
+{
+	public Color color = Color.black;
+	public float distance;
+
+	void OnDrawGizmos()
+	{
+		RaycastHit hitInfo;
+
+		Gizmos.color = color;
+		Gizmos.DrawCube( transform.position, 0.01f*Vector3.one);
+
+		if( Physics.Raycast( this.transform.position, this.transform.forward, out hitInfo ) )
+		{
+			distance = hitInfo.distance;
+			Gizmos.DrawLine( this.transform.position, hitInfo.point );
+			Gizmos.DrawRay( hitInfo.point, 0.05f*(-this.transform.up-this.transform.forward) );
+
+			if( Physics.Raycast( this.transform.position, -this.transform.forward, out hitInfo ) )
+			{
+				Gizmos.DrawLine( this.transform.position, hitInfo.point );
+				Gizmos.DrawRay( hitInfo.point, 0.05f*(-this.transform.up+this.transform.forward) );
+
+				distance += hitInfo.distance;
+
+				distance = Mathf.Round( 1000f*distance );
+
+				EditorText.DrawText( transform.position - 0.01f*transform.up, "" + (distance) + "mm", color );
+			}
+		}
+		
+	}
+}
+
+public class EditorText
+{
+	public static void DrawText( Vector3 position, string text, Color color )
+	{
+		GUIStyle style = new GUIStyle();
+		style.normal.textColor = color;
+		Handles.Label( position, text, style );
+	}
+		
+}
